d68fabcd55e8dcc1e48a14ba84348adaf2f83b4b
oquendogs <oquendogs@gmail.com>
2023-07-04 12:26:31 -0300

    proyecto final

---
+++ main.js
@@ -0,0 +1,6 @@
+const boton = document.querySelector('.boton');
+
+boton.addEventListener('click', () => {
+    boton.innerHTML = `<a href="./imagenes/2023julCVGermanOquendo.pdf" target="_blank">Descargar CV</a>`
+})
+
